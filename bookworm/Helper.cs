f723459f4f48ae7aae523d3c1d1ccd81c42135a2
buraksenyurt <burakselimsenyurt@gmail.com>
2025-06-13 17:43:43 +0300

    Added a helper class

---
+++ bookworm/Helper.cs
@@ -0,0 +1,45 @@
+namespace bookworm;
+
+public static class Helper
+{
+    public static void ShowMessage(MessageType messageType, string[] messages)
+    {
+        if (messages == null || messages.Length == 0)
+            return;
+
+        string message = string.Join("\n", messages);
+        var originalColor = Console.ForegroundColor;
+
+        ConsoleColor newColor;
+        string prefix;
+
+        switch (messageType)
+        {
+            case MessageType.Info:
+                newColor = ConsoleColor.Red;
+                prefix = "Info";
+                break;
+            case MessageType.Warning:
+                newColor = ConsoleColor.Yellow;
+                prefix = "Warning";
+                break;
+            case MessageType.Error:
+                newColor = ConsoleColor.Green;
+                prefix = "Error";
+                break;
+            default:
+                throw new ArgumentOutOfRangeException(nameof(messageType), messageType, null);
+        }
+
+        Console.ForegroundColor = newColor;
+        Console.WriteLine($"{prefix}: {message}");
+        Console.ForegroundColor = originalColor;
+    }
+}
+
+public enum MessageType
+{
+    Info,
+    Warning,
+    Error
+}
