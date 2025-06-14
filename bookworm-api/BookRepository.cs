f177fa0f21444d5b47dcd3ec0867dea2b4ce5665
buraksenyurt <burakselimsenyurt@gmail.com>
2025-06-14 21:33:32 +0300

    Added exception handling with logger

--- bookworm-api/BookRepository.cs
+++ bookworm-api/BookRepository.cs
@@ -1,5 +1,6 @@
 using Dapper;
 using Microsoft.Data.Sqlite;
+using Serilog;
 
 namespace bookworm_api;
 
@@ -10,28 +11,60 @@ public class BookRepository(string connStr)
 
     public async Task AddAsync(Book book)
     {
-        using var conn = new SqliteConnection(_connStr);
-        await conn.ExecuteAsync(@"INSERT INTO Books (Title, Category, Read, Created, Modified)
+        try
+        {
+            using var conn = new SqliteConnection(_connStr);
+            await conn.ExecuteAsync(@"INSERT INTO Books (Title, Category, Read, Created, Modified)
                     VALUES (@Title,@Category,@Read,@Created,@Modified)", book);
+        }
+        catch (Exception ex)
+        {
+            Log.Error(ex, "An error occured while adding book");
+            throw;
+        }
     }
     public async Task DeleteAsync(int id)
     {
-        using var conn = new SqliteConnection(_connStr);
-        await conn.ExecuteAsync("DELETE FROM Books Where Id = @id", new { id });
+        try
+        {
+            using var conn = new SqliteConnection(_connStr);
+            await conn.ExecuteAsync("DELETE FROM Books Where Id = @id", new { id });
+        }
+        catch (Exception ex)
+        {
+            Log.Error(ex, "An error occured while deleting book");
+            throw;
+        }
     }
 
     public async Task<Book?> GetByTitleAsync(string title)
     {
-        using var conn = new SqliteConnection(_connStr);
-        var row = await conn.QueryAsync("SELECT Id, Title, Category, Read, Created, Modified FROM Books WHERE LOWER(Title) = LOWER(@title)", new { title });
-        return row.Select(MapTo()).FirstOrDefault();
+        try
+        {
+            using var conn = new SqliteConnection(_connStr);
+            var row = await conn.QueryAsync("SELECT Id, Title, Category, Read, Created, Modified FROM Books WHERE LOWER(Title) = LOWER(@title)", new { title });
+            return row.Select(MapTo()).FirstOrDefault();
+        }
+        catch (Exception ex)
+        {
+            Log.Error(ex, "An error occured while getting book from title");
+            throw;
+        }
     }
 
     public async Task<IEnumerable<Book>> GetAllAsync()
     {
-        using var conn = new SqliteConnection(_connStr);
-        var rows = await conn.QueryAsync("SELECT Id, Title, Category, Read, Created, Modified FROM Books");
-        return rows.Select(MapTo());
+        try
+        {
+            using var conn = new SqliteConnection(_connStr);
+            var rows = await conn.QueryAsync("SELECT Id, Title, Category, Read, Created, Modified FROM Books");
+            return rows.Select(MapTo());
+        }
+        catch (Exception ex)
+        {
+            Log.Error(ex, "An error occured while fetching all books");
+            throw;
+        }
     }
 
     private static Func<dynamic, Book> MapTo()
--- bookworm-api/Program.cs
+++ bookworm-api/Program.cs
@@ -1,18 +1,49 @@
 using bookworm_api;
+using Microsoft.AspNetCore.Diagnostics;
+using Serilog;
+using Serilog.Events;
 
 var builder = WebApplication.CreateBuilder(args);
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console(restrictedToMinimumLevel: LogEventLevel.Information)
+    .WriteTo.File("logs/errors.log", restrictedToMinimumLevel: LogEventLevel.Error)
+    .CreateLogger();
+
 var connStr = builder.Configuration.GetConnectionString("DbConStr") ?? "Data Source = books.db";
 Database.Initialize(connStr);
 builder.Services.AddSingleton<IBookRepository>(new BookRepository(connStr));
 
 var app = builder.Build();
 
+app.UseExceptionHandler(app =>
+{
+    app.Run(async context =>
+    {
+        var logger = context.RequestServices.GetRequiredService<ILogger<Program>>();
+        var exceptionHandlerPathFeature = context.Features.Get<IExceptionHandlerPathFeature>();
+
+        context.Response.StatusCode = StatusCodes.Status500InternalServerError;
+        context.Response.ContentType = "application/json";
+
+        await context.Response.WriteAsJsonAsync(new
+        {
+            error = "Something went wrong. Please try again later."
+        });
+    });
+});
+
 app.MapGet("/", () => "Bookworms Db Store Api");
 
 app.MapPost("/books", async (BookDto dto, IBookRepository repository) =>
 {
+    var isExist = await repository.GetByTitleAsync(dto.Title);
+    if (isExist != null)
+    {
+        return Results.BadRequest(new { message = $"Book with title '{dto.Title}' already exist" });
+    }
     var book = new Book(0, dto.Title, dto.Category, dto.Read, DateTime.UtcNow, null);
     await repository.AddAsync(book);
+    Log.Information($"'{dto.Title}' added to deppo successfully");
     return Results.Created($"/books", dto);
 });
 
@@ -28,6 +59,7 @@ app.MapGet("/books/{title}", async (string title, IBookRepository repository) =>
     var book = await repository.GetByTitleAsync(title);
     if (book is null)
     {
+        Log.Warning($"Book with title '{title}' not found");
         return Results.NotFound(new { message = $"Book with title '{title}' not found :/" });
     }
     return Results.Ok(new BookDto(book.Id, book.Title, book.Category, book.Read));
@@ -38,6 +70,7 @@ app.MapDelete("/books/{title}", async (string title, IBookRepository repository)
     var book = await repository.GetByTitleAsync(title);
     if (book is null)
     {
+        Log.Warning($"Book with title '{title}' not found");
         return Results.NotFound(new { message = $"Book with title '{title}' not found :/" });
     }
     await repository.DeleteAsync(book.Id);
