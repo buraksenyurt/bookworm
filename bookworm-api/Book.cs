dc3efd874d55df8ead3d9cfb50ea0e83e09ccab7
buraksenyurt <burakselimsenyurt@gmail.com>
2025-06-14 19:38:29 +0300

    Initiated api project

---
+++ bookworm-api/Book.cs
@@ -0,0 +1,8 @@
+﻿namespace bookworm_api;
+
+public class Book
+{
+    public required string Title { get; set; }
+    public required string Category { get; set; }
+    public required bool Read { get; set; }
+}
