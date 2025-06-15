ed7fd23feefcf75fb74b48fed5da782fb6a3170a
buraksenyurt <burakselimsenyurt@gmail.com>
2025-06-15 16:11:43 +0300

    Added http client component

---
+++ bookworm/Client/BookwormApiClient.cs
@@ -0,0 +1,37 @@
+using System.Net.Http.Json;
+using Serilog;
+
+namespace bookworm.Client;
+
+public class BookwormApiClient(HttpClient httpClient)
+    : IBookwormApiClient
+{
+    public async Task AddAsync(Book book, CancellationToken cancellationToken)
+    {
+        var response = await httpClient.PostAsJsonAsync("/books", book, cancellationToken);
+        if (!response.IsSuccessStatusCode)
+        {
+            var body = await response.Content.ReadAsStringAsync(cancellationToken);
+            Log.Error("Failed to add book: {Response}", body);
+            throw new HttpRequestException($"'{response.StatusCode}' Book could not be added.");
+        }
+    }
+
+    public async Task<IEnumerable<Book>> GetAllAsync(CancellationToken cancellationToken)
+    {
+        var response = await httpClient.GetAsync("/books", cancellationToken);
+        response.EnsureSuccessStatusCode();
+        return await response.Content.ReadFromJsonAsync<IEnumerable<Book>>(cancellationToken) ?? [];
+    }
+
+    public async Task RemoveAsync(string title, CancellationToken cancellationToken)
+    {
+        var response = await httpClient.DeleteAsync($"/books/{Uri.EscapeDataString(title)}", cancellationToken);
+        if (!response.IsSuccessStatusCode)
+        {
+            var body = await response.Content.ReadAsStringAsync(cancellationToken);
+            Log.Error("Failed to remove book: {Response}", body);
+            throw new HttpRequestException($"'{response.StatusCode}' Book could not be removed.");
+        }
+    }
+}
---
+++ bookworm/Client/IBookwormApiClient.cs
@@ -0,0 +1,8 @@
+namespace bookworm.Client;
+
+public interface IBookwormApiClient
+{
+    Task<IEnumerable<Book>> GetAllAsync(CancellationToken cancellationToken);
+    Task AddAsync(Book book, CancellationToken cancellationToken);
+    Task RemoveAsync(string title, CancellationToken cancellationToken);
+}
