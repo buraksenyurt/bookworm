d09a2fefdf59c4170e27c7bebc78d9bcc304ca77
buraksenyurt <burakselimsenyurt@gmail.com>
2025-06-17 23:40:11 +0300

    Added more unit test methods.

---
+++ bookworm-cli.UnitTests/Commands/Book/ListCommandTests.cs
@@ -0,0 +1,64 @@
+﻿using Commands.Book;
+using Moq;
+using Services;
+using System.CommandLine;
+
+namespace bookworm_cli.UnitTests.Commands.Book;
+
+public class ListCommandTests
+{
+    private readonly Mock<IBookwormService> _bookwormServiceMock;
+    private readonly ListCommand _command;
+
+    public ListCommandTests()
+    {
+        _bookwormServiceMock = new Mock<IBookwormService>();
+        _command = new ListCommand(_bookwormServiceMock.Object, "list", "List of books");
+    }
+    [Fact]
+    public async Task ShouldPrintBooks_WhenBooksAreReturedFromService()
+    {
+        var books = new List<bookworm_cli.Book>{
+            new()
+            {
+                Title= "Programming With C#",
+                Category = "Technical-Books",
+                Read=true
+            },
+            new(){
+                Title= "Denizler Altında Yirmibin Fersah",
+                Category = "Fiction",
+                Read=true
+            }
+        };
+        _bookwormServiceMock
+            .Setup(s => s.GetAllBooksAsync(It.IsAny<CancellationToken>()))
+            .ReturnsAsync(books);
+
+        using var consoleOutput = new StringWriter();
+        Console.SetOut(consoleOutput);
+
+        var _ = await _command.InvokeAsync("");
+
+        var output = consoleOutput.ToString();
+        Assert.Contains("Programming With C#", output);
+        Assert.Contains("Denizler Altında Yirmibin Fersah", output);
+    }
+
+    [Fact]
+    public async Task ShouldPrintNoBooks_WhenBooksAreReturedEmptyFromService()
+    {
+        var noBooks = new List<bookworm_cli.Book>();
+        _bookwormServiceMock
+            .Setup(s => s.GetAllBooksAsync(It.IsAny<CancellationToken>()))
+            .ReturnsAsync(noBooks);
+
+        using var consoleOutput = new StringWriter();
+        Console.SetOut(consoleOutput);
+
+        var _ = await _command.InvokeAsync("");
+
+        var output = consoleOutput.ToString();
+        Assert.Contains("No books found.", output);
+    }
+}
---
+++ bookworm-cli.UnitTests/Commands/Book/RemoveCommandTests.cs
@@ -0,0 +1,50 @@
+﻿using Commands.Book;
+using Moq;
+using Services;
+using System.CommandLine;
+
+namespace bookworm_cli.UnitTests.Commands.Book;
+
+public class RemoveCommandTests
+{
+    private readonly Mock<IBookwormService> _bookwormServiceMock;
+    private readonly RemoveCommand _command;
+
+    public RemoveCommandTests()
+    {
+        _bookwormServiceMock = new Mock<IBookwormService>();
+        _command = new RemoveCommand(_bookwormServiceMock.Object, "remove", "Remove a book from store");
+    }
+
+    [Fact]
+    public async Task ShouldReturnError_WhenTitleIsNullOrEmpty()
+    {
+        var actual = await Record.ExceptionAsync(() => _command.InvokeAsync("--title\"\" --category \"Fiction\" --read true"));
+        _bookwormServiceMock.Verify(
+            s => s.RemoveBookAsync(
+                It.IsAny<string>(),
+                It.IsAny<CancellationToken>()),
+            Times.Never);
+    }
+
+    [Fact]
+    public async Task ShouldLogError_WhenServiceThrowsException()
+    {
+        var title = "System Programming with Rust";
+
+        _bookwormServiceMock.Setup(
+            s => s.RemoveBookAsync(
+                 It.IsAny<string>(),
+                It.IsAny<CancellationToken>()
+                ))
+            .ThrowsAsync(new Exception("Service Unavailable"));
+
+        var result = await _command.InvokeAsync($"--title \"{title}\"");
+
+        _bookwormServiceMock.Verify(
+            s => s.RemoveBookAsync(
+                title,
+                It.IsAny<CancellationToken>())
+            , Times.Once);
+    }
+}
